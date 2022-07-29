f73fdda8a1791fd01bbe74fa50a061e185c09efe
russelldnn <russelldnn@gmail.com>
2022-07-29 07:39:03 -0500

    navbar attempted

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Navi from "./Navbar";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function Header(props) {
+  return (
+    <Navbar collapseOnSelect className="navbar" variant="dark" expand="sm" aria-current="true">
+      <Navbar.Brand href="/portfolio-React" className="name">
+        {" "}
+        &lt; Home /&gt;
+      </Navbar.Brand>
+      
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      
+      <Navbar.Collapse
+        id="responsive-navbar-nav"
+        className=" justify-content-end"
+      >
+        <Navi setCurrentPage={props.setCurrentPage}></Navi>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
